97c3bab5be1e2f43c70997145ad307668610f432
mrinal1224 <iammrinalkc@gmail.com>
2024-09-18 09:34:44 +0530

    push

---
+++ Class-10 (Introduction to JS)/script.js
@@ -0,0 +1,24 @@
+// console.log("hello")
+
+// Variables
+
+let a = 10
+
+let b = 'Scaler'
+
+let c = true
+
+let d = 's'
+
+
+const r = 3
+
+ r = 5
+
+
+a = 20
+console.log(a)
+console.log(b)
+console.log(b)
+console.log(r)
+

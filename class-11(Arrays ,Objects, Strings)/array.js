f0d10850ee0a0b7878eb8e915d353f23fd59631b
mrinal1224 <iammrinalkc@gmail.com>
2024-09-30 09:41:37 +0530

    push

--- class-11(Arrays ,Objects, Strings)/array.js
+++ class-11(Arrays ,Objects, Strings)/array.js
@@ -1,24 +1,80 @@
-let cars = ['Ferrari' , 'Mustang' , 'Rolls Royce']
+let cars = ['Ferrari' , 'Mustang' , 'Rolls Royce' , 'Mercedes']
 
 // push method
 
-cars.push('Bently')
+// cars.push('Bently')
 
-console.log(cars)
+// console.log(cars)
 
-// pop method
+// // pop method
 
-console.log(cars.pop())
-console.log(cars.pop())
+// console.log(cars.pop())
+// console.log(cars.pop())
 
-console.log(cars)
+// console.log(cars)
 
-// unshift
+// // unshift
 
-cars.unshift('BMW')
+// cars.unshift('BMW')
 
-console.log(cars)
+// console.log(cars)
 
-// shift
-cars.shift()
-console.log(cars)
+// // shift
+// cars.shift()
+// console.log(cars)
+
+// // includes
+
+// let doesExists = cars.includes('Mercedes')
+// console.log(doesExists)
+
+// // indexOf
+
+// let index = cars.indexOf('Mustang')
+// console.log(index)
+
+
+// slice
+
+// const slicedCars = cars.slice(1 , 3)
+
+// console.log(slicedCars)
+
+// splice
+
+// const splicedArr = cars.splice(1 , 2)
+// console.log(splicedArr)// [Mustang , rolls Royce]
+
+// console.log(cars) // ['Ferrari' , 'Mercedes']
+
+// Merge an Array
+
+// let cars2 = ['Urus' , 'Range Rover']
+
+//concat
+
+// let concatArr = cars.concat(cars2)
+// console.log(concatArr)
+
+
+// reverse
+
+// let reversedArr = cars.reverse()
+// console.log(reversedArr)
+
+// join 
+
+let carString = cars.join(' , ')
+
+console.log(carString)
+
+
+
+
+
+
+
+
+
+
+

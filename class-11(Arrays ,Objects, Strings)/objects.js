56281a102b9dac02c3f551e49084242256429ebf
mrinal1224 <iammrinalkc@gmail.com>
2024-09-30 09:53:24 +0530

    push

---
+++ class-11(Arrays ,Objects, Strings)/objects.js
@@ -0,0 +1,19 @@
+let person1 = {
+    name: 'Kanishk',
+    age : 18,
+    ownsAcar : true
+}
+
+let person2 = {
+    name: 'Mrinal',
+    age : 25,
+    ownsAcar : false
+}
+console.log(person1)
+console.log(person2)
+
+// Working with an Object
+
+
+
+

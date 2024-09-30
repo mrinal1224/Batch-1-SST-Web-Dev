5c2bbb4ce5161b9bd520e8761ebe7efb08a74c62
mrinal1224 <iammrinalkc@gmail.com>
2024-09-30 09:01:52 +0530

    push

---
+++ class-11(Arrays ,Objects, Strings)/array.js
@@ -0,0 +1,24 @@
+let cars = ['Ferrari' , 'Mustang' , 'Rolls Royce']
+
+// push method
+
+cars.push('Bently')
+
+console.log(cars)
+
+// pop method
+
+console.log(cars.pop())
+console.log(cars.pop())
+
+console.log(cars)
+
+// unshift
+
+cars.unshift('BMW')
+
+console.log(cars)
+
+// shift
+cars.shift()
+console.log(cars)

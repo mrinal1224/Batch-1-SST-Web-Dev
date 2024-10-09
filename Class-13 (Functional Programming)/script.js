90f26e93c5618abd8385fc82781ca06f7ef17e34
mrinal1224 <iammrinalkc@gmail.com>
2024-10-09 09:42:41 +0530

    push

--- Class-13 (Functional Programming)/script.js
+++ Class-13 (Functional Programming)/script.js
@@ -1,13 +1,57 @@
-let radiuses = [1 ,2 ,4 ,5 ,6]
+
 
 // Write three functions to calculate the 
 // area , circumferene and diameter 
 // of the circle for these Radiuses
 // Create functions to do this
 // Create new arrays to store these values
-// originak array should not be mutated
+// original array should not be mutated
 
 // You should have three respective arrays 
 // area - []
 // circumfernce - []
 // diameter = []
+let MyRadius = [1 ,2 ,4 ,5 ,4]
+function calculateArea(radiusArr){
+    let areas = []
+
+    for(let i=0 ; i<radiusArr.length ; i++){
+        areas.push(3.14*radiusArr[i]*radiusArr[i])
+    }
+
+    return areas
+}
+
+
+let finalAreas= calculateArea(MyRadius)
+console.log(finalAreas)
+
+// Diameters and circumference
+
+function calculateDiameter(radiusArr){
+    let diameters = []
+
+    for(let i=0 ; i<radiusArr.length ; i++){
+        diameters.push(2*radiusArr[i])
+    }
+
+    return diameters
+}
+
+let finaldiameters= calculateDiameter(MyRadius)
+console.log(finaldiameters)
+
+
+
+function calculateCircumference(radiusArr){
+    let circumferences = []
+
+    for(let i=0 ; i<radiusArr.length ; i++){
+        circumferences.push(2*radiusArr[i])
+    }
+
+    return circumferences
+}
+
+let finalCircumferences= calculateCircumference(MyRadius)
+console.log(finalCircumferences)

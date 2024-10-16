bccbd7ae3db47fd3df22e80b6dd815027f4f6d91
mrinal1224 <iammrinalkc@gmail.com>
2024-10-16 10:10:40 +0530

    push

--- Class-14 (Map Filter Reduce)/reduce.js
+++ Class-14 (Map Filter Reduce)/reduce.js
@@ -2,3 +2,24 @@ let nums = [1 , 2 ,3 ,4 ,5 ,6]
 
 // calculate the sum of all elements in the array
 // Write Imperative code for this
+
+function calculateSum(arr){
+    let sum = 0
+
+    for(let i=0 ; i<arr.length ; i++){
+        sum = sum+arr[i]
+    }
+    return sum
+}
+let total = calculateSum(nums)
+console.log(total)
+
+// reduce
+
+let totalSum = nums.reduce(function(acc , currVal){
+    acc = acc+currVal
+    return acc
+}, 0)
+
+console.log(totalSum)
+
